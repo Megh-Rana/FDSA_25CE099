e17a47bd785c5b03516fcb8650be2dec09302260
Megh Rana <meghthedev@gmail.com>
2026-07-22 09:39:55 +0530

    practicals: add practical list * and practical 2.1 iterative approach

---
+++ pr2/2_1_it.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int find_iterative(vector<int> nums,int target)
+{
+    for(int i=0;i<nums.size();i++)
+    {
+        if(target==nums[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n,target;
+    cin >> n;
+ 
+    vector<int> nums;
+    for(int i=0;i<n;i++)
+    {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
+
+   cin >> target;
+    int result=find_iterative(nums,target);
+    if(result!=-1)
+    {
+        cout << result << endl;
+        cout << result+1;        
+    }
+    else
+    {
+        cout << "Element " << target << " is not present in the array"<< endl;
+        cout << nums.size();
+    }
+    
+    
+}
+
